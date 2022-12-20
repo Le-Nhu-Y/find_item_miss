828b3658a40c8bedb41226bec131e19aeefc9bd0
Le-Nhu-Y <lenhuy20021989@gmail.com>
2022-12-20 11:05:17 +0700

    Add test.ts

--- test.js
+++ test.js
@@ -1,14 +1,28 @@
-function find_item_miss(data) {
-    var i = 1;
-    var arr = [];
-    while (i <= 10) {
-        if (data.indexOf(i) == -1) {
-            arr.push(i);
+function find_first_occurrence(numbers, target) {
+    var low = 0;
+    var high = numbers.length - 1;
+    var result = -1;
+    while (low <= high) {
+        var mid = Math.floor((low + high) / 2);
+        if (numbers[mid] == target) {
+            result = mid;
+            high = mid - 1;
         }
-        i++;
+        else if (numbers[mid] > target) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
     }
-    return arr;
+    return result;
 }
-;
-var arr = [1, 2, 5, 6, 7, 10];
-console.log(find_item_miss(arr));
+var data = [2, 5, 5, 5, 6, 6, 8, 9, 9, 9];
+var target = 5;
+var index = find_first_occurrence(data, target);
+if (index !== -1) {
+    console.log("Ph\u1EA7n t\u1EED ".concat(target, " xu\u1EA5t hi\u1EC7n \u0111\u1EA7u ti\u00EAn t\u1EA1i v\u1ECB tr\u00ED ").concat(index));
+}
+else {
+    console.log("Ph\u1EA7n t\u1EED ".concat(target, " kh\u00F4ng t\u1ED3n t\u1EA1i trong m\u1EA3ng"));
+}
--- test.ts
+++ test.ts
@@ -1,14 +1,29 @@
-function find_item_miss(data: number[]): number[] {
-    let i: number = 1;
-    let arr: number[] = [];
-    while (i <= 10) {
-        if (data.indexOf(i) == -1) {
-            arr.push(i);
-        }
-        i++;
-    }
-    return arr;
-};
-let arr = [1, 2, 5, 6, 7, 10];
-console.log(find_item_miss(arr))
+function find_first_occurrence(numbers: number[],
+                             target: number): number {
+    let low: number = 0;
+    let high: number = numbers.length - 1;
+    let result: number  = -1;
 
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (numbers[mid] == target) {
+            result = mid;
+            high = mid - 1;
+        } else if (numbers[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return result;
+}
+
+let data: number[] = [2, 5, 5, 5, 6, 6, 8, 9, 9, 9];
+let target: number = 5
+let index: number = find_first_occurrence(data, target);
+
+if (index !== -1) {
+    console.log(`Phần tử ${target} xuất hiện đầu tiên tại vị trí ${index}`);
+} else {
+    console.log(`Phần tử ${target} không tồn tại trong mảng`)
+}

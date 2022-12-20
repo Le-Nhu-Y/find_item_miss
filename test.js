d0c2474863d86ffd445132ef6d4d7d98e65d1e0c
Le-Nhu-Y <lenhuy20021989@gmail.com>
2022-12-20 10:56:13 +0700

    Add test.ts file

---
+++ test.js
@@ -0,0 +1,14 @@
+function find_item_miss(data) {
+    var i = 1;
+    var arr = [];
+    while (i <= 10) {
+        if (data.indexOf(i) == -1) {
+            arr.push(i);
+        }
+        i++;
+    }
+    return arr;
+}
+;
+var arr = [1, 2, 5, 6, 7, 10];
+console.log(find_item_miss(arr));
